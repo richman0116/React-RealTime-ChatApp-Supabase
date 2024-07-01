bea0eb9e5a1e37fd00caa9cb4fa5a63ecd68e06c
richman0116 <maria8partee@gmail.com>
2024-07-01 06:48:10 -0500

    add user avatar with default image

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,92 +1,80 @@
-import { Box, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Flex, Text, Image } from "@chakra-ui/react";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
-import { MdVerified } from "react-icons/md";
 import { truncateText } from "../utils";
 
 export default function Message({ message, isYou }) {
 
-  const countyCode =
-    message.country && message.country !== "undefined"
-      ? message.country.toLowerCase()
-      : "";
+  function formatTimestampTo12Hour(timestamp) {
+    // Create a new Date object from the timestamp
+    const date = new Date(timestamp);
+
+    // Get the hours and minutes
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    // Determine AM or PM
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+
+    // Convert hours from 24-hour format to 12-hour format
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+
+    // Pad minutes with leading zero if needed
+    const minutesStr = minutes < 10 ? `0${minutes}` : minutes;
+
+    // Return the formatted time string
+    return `${hours}:${minutesStr} ${ampm}`;
+  }
 
   return (
-    <Box display="grid" justifyItems={isYou ? "end" : "start"}>
-      <Grid
-        templateRows={isYou ? "1fr 20px" : "30px 1fr 25px"}
-        templateColumns="1fr"
-        maxW={isYou ? "80%" : "70%"}
-        w="auto"
-        px="3"
-        py="2"
-        borderRadius="15px"
-        borderTopLeftRadius={isYou ? "15px" : "0"}
-        borderTopRightRadius={isYou ? "0" : "15px"}
-        bg={isYou ? "#0e3b82" : "#292929"}
-        mt="5"
-        position="relative"
-        _after={{
-          position: "absolute",
-          content: "''",
-          width: 0,
-          height: 0,
-          borderStyle: "solid",
-          borderWidth: isYou ? "0px 0px 10px 10px" : "0px 10px 10px 0",
-          borderColor: isYou
-            ? "transparent transparent transparent #0e3b82"
-            : "transparent #292929 transparent transparent",
-          top: 0,
-          left: isYou ? "auto" : "-10px",
-          right: isYou ? "-10px" : "auto",
-        }}
-      >
-        {!isYou && (
-          <GridItem
-            fontWeight="500"
-            fontSize="md"
-            justifySelf="start"
-            color="gray"
-            mb="2"
+    <Flex display={"flex"} direction={'row'} gap={4} mt={5}>
+      <Box display={isYou ? "none" : "block"}><Image src="../../public/default.webp" rounded={'full'} width={'8'} height={'7'}/></Box>
+      <Flex display={"flex"} width={'full'} direction={'column'}>
+        <Box display="grid" justifyItems={isYou ? "end" : "start"}>
+          {!isYou ? <Text fontSize="10px">{message.username + ", " + formatTimestampTo12Hour(message.timestamp)}</Text> : <Text fontSize="10px">{formatTimestampTo12Hour(message.timestamp)}</Text>}
+        </Box>
+        <Box display="grid" justifyItems={isYou ? "end" : "start"} width={'full'}>
+          <Grid
+            templateColumns="1fr"
+            maxW="85%"
+            w="auto"
+            pl="3"
+            pr="4"
+            py="2"
+            borderRadius="15px"
+            borderTopLeftRadius={isYou ? "15px" : "0"}
+            borderTopRightRadius={isYou ? "0" : "15px"}
+            bg={isYou ? "#0e3b82" : "#292929"}
+            position="relative"
+            _after={{
+              position: "absolute",
+              content: "''",
+              width: 0,
+              height: 0,
+              borderStyle: "solid",
+              borderWidth: isYou ? "0px 0px 10px 10px" : "0px 10px 10px 0",
+              borderColor: isYou
+                ? "transparent transparent transparent #0e3b82"
+                : "transparent #292929 transparent transparent",
+              top: 0,
+              left: isYou ? "auto" : "-10px",
+              right: isYou ? "-10px" : "auto",
+            }}
           >
-            <span>{message.username} </span>
-            {message.is_authenticated && (
-              <MdVerified
-                color="#1d9bf0"
-                style={{ display: "inline", marginRight: "5px" }}
-              />
-            )}
-            {countyCode && (
-              <Box display="inline-block" fontSize="10px">
-                from {message.country}{" "}
-                <img
-                  style={{ display: "inline-block", marginTop: "-4px" }}
-                  src={`/flags/${countyCode}.png`}
-                  alt={message.country}
-                />
-              </Box>
-            )}
-          </GridItem>
-        )}
-        <GridItem
-          justifySelf="start"
-          textAlign="left"
-          wordBreak="break-word"
-          fontSize="md"
-          fontFamily="Montserrat, sans-serif"
-        >
-          {truncateText(message.text)}
-        </GridItem>
-        <GridItem
-          color="gray"
-          fontSize="10px"
-          justifySelf="end"
-          alignSelf="end"
-        >
-          {dayjs(message.timestamp).fromNow()}
-        </GridItem>
-      </Grid>
-    </Box>
+            <GridItem
+              justifySelf="start"
+              textAlign="left"
+              wordBreak="break-word"
+              fontSize="md"
+              fontFamily="Montserrat, sans-serif"
+            >
+              {truncateText(message.text)}
+            </GridItem>
+          </Grid>
+        </Box>
+      </Flex>
+    </Flex>
   );
 }
